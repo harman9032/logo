14b37245ed76acd4060fb3573e77f28a87953429
Harmanpreet Singh <88335184+harman9032@users.noreply.github.com>
2025-11-13 02:25:01 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import PrivacyPolicy from './components/PrivacyPolicy';
 import CookiesPolicy from './components/CookiesPolicy';
 import Terms from './components/Terms';
 import ThankYou from './components/ThankYou';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/cookies-policy" element={<CookiesPolicy />} />
-      <Route path="/terms" element={<Terms />} />
-      <Route path="/thank-you" element={<ThankYou />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/cookies-policy" element={<CookiesPolicy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+    </>
   );
 }
